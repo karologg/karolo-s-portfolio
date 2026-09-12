d872471db858f9adc735e44d4d4ed1c0c9489bca
karolo <karoloarchive@gmail.com>
2026-09-12 18:32:39 -0300

    commit progressbar update

--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,12 @@
 const bars = document.querySelectorAll(".progress");
 
-const observer= new IntersectionObserver(entries => {
+const observer= new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
+            
             entry.target.classList.add("animate");
+        } else {
+            entry.target.classList.remove("animate");
         }
     });
 });
